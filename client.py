5c16e8c03205d4e7bc7a19d64f9532866ec87463
chongyangshi <chongyang.shi.uk@gmail.com>
2014-10-14 22:45:48 +0100

    more work with message wrapping
    outbound message wrapper ready, client ID improvements, config etc.

--- client.py
+++ client.py
@@ -1,6 +1,10 @@
 from sys import exit
+from os import urandom
 import re
 import json
+from base64 import b64encode
+import socket
+from hashlib import sha1
 import client_encrypt
 import client_decrypt
 import client_servercontact
@@ -13,18 +17,46 @@ with open('client_config.json') as clientConfigJSONFile:
     print "Client ID is not a standard email address, please check client_config.json."
     exit(0)
   clientkey = clientConfigJSONData['client_secure_key']
-  serverip = clientConfigJSONData['server_ip'] #Todo: validating server IP
-  serverport = clientConfigJSONData['server_port'] #Todo: validating server port
+  serverip = clientConfigJSONData['server_ip']
+  try:
+    socket.inet_aton(serverip)
+  except socket.error:
+    try:
+      socket.inet_pton(socket.AF_INET6, serverip)
+    except socket.error:
+      print "Server IP not in correct shape, please check config."
+      exit(0)
+  serverport = int(clientConfigJSONData['server_port'])
+  if not (0 < serverport < 65536):
+    print "Server port must be between 1-65535, please check config."
+    exit(0)
   client = {'clientID': clientid,'clientEncryptionKey':clientkey,'clientServerIP':serverip,'clientServerPort':serverport} #Todo: server connections
-
   pseudoUsername = str(client['clientID'].split('@')[0])
+  clientIDForHash = client['clientID']+sha1(urandom(10)).hexdigest()
+  clientHashedID = sha1(clientIDForHash).hexdigest() #shall be validated at the server for a length of 40, regulated
+  print "Your current address hash is",clientHashedID,". You will use it to send and receive messages on this session."
 
-  #for testing only
-  #cptxt,IVIV,origlen = client_encrypt.encrypt(client['clientEncryptionKey'], "jadn3ui2jk23.4@$@#$#@3488329dk,x.,d.x.;[p['wd.ekmn23']]'")
-  #clrtxt = client_decrypt.decrypt(client['clientEncryptionKey'],IVIV,origlen,cptxt)
+recipientAddressSet = False
 
 while True:
-  print pseudoUsername,": ",
-  clientNewMessage = raw_input()
-  clientEncryptedMessage, clientEncryptedIV, clientMessageOL = client_encrypt.encrypt(client['clientEncryptionKey'], clientNewMessage)
-  clientPushReturn = client_servercontact.pushMessage(clientEncryptedMessage, clientEncryptedIV, clientMessageOL) #Module not yet coded!!
+  if not recipientAddressSet:
+    print "Send Message To Hashed Address: ",
+    recipientAddress = raw_input()
+    if re.match("^[A-Za-z0-9_-]+$", recipientAddress):
+      recipientAddressSet = True
+    else:
+      recipientAddressSet = False
+      print "Invalid recipient address, it should only contain numbers and letters."
+
+  if recipientAddressSet == True:
+    print pseudoUsername,"to",recipientAddress,":",
+    clientNewMessage = raw_input()
+
+    if clientNewMessage == '/quit':
+      recipientAddressSet = False
+      print "Press Ctrl+D to exit program, enter a new recipient address to send a message to another user."
+      continue
+
+    clientEncryptedMessage, clientEncryptedIV, clientMessageOL = client_encrypt.encrypt(client['clientEncryptionKey'], clientNewMessage)
+    clientPushReturn = client_servercontact.parsingMessage(clientHashedID, recipientAddress, b64encode(clientEncryptedMessage), b64encode(clientEncryptedIV), clientMessageOL)
+    print clientPushReturn
--- client_servercontact.py
+++ client_servercontact.py
@@ -1,2 +1,5 @@
-def pushMessage(clientkey, clientmessage, clientmessageOL):
-  return True #Todo: write this module.
+import json
+def parsingMessage(clienthash, recipienthash, clientmessage, clientiv, clientmessageOL):
+  outboundMessage = {'recipient':recipienthash, 'sender':clienthash, 'messagebody': clientmessage, 'messageiv': clientiv, 'messagelength': clientmessageOL}
+  outboundMessageJSON = json.dumps(outboundMessage)
+  return outboundMessageJSON
